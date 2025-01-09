3e6228d5d07fe388a2f30671dc25c86593d56c4f
Shark899-droid <fontanaedera@yahoo.com>
2025-01-09 17:42:24 +0100

    Last

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken'
 export const authorization = async (req, res, next) => {
   const token = req.headers.authorization
-  if (!token) {
-    res.status(400).json({ message: 'Token not present please validate' })
-  }
   const decodedToken = token.split(' ')[1]
+  if (!decodedToken) {
+    throw new Error("Token not present plaese validate")
+  }
   if (!jwt.verify(decodedToken, process.env.JWT_SECRET)) {
     res.status(400).json({ message: 'Token not valid' })
   }
